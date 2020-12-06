5ab3a0c6e832f2f80da9b4b7ea93c2301fc95943
ryan-li20 <rli20@stuy.edu>
2020-12-06 16:03:27 -0500

    Tested rocketChat + works

---
+++ Range.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Range implements IntegerSequence{
+  private int start,end,current;
+
+  //@param start : the starting value (inclusive) which must be <= end.
+  //@param end : the ending value which is also inclusive.
+  public Range(int s,  int e){
+    start = s;
+    end = e;
+    current = s;
+  }
+
+  public void reset(){
+    current = start;
+  }
+
+  public int length(){
+    return (end - start) + 1;
+  }
+
+  //When current is no longer a valid element in the range, it should return false.
+  public boolean hasNext(){
+    return current <= end;
+  }
+
+  //@throws NoSuchElementException when hasNext() is false.
+  //This will return the current value, it will also increase current value by 1.
+  //e.g.  if current is 5. This will make current 6, and return 5.
+  public int next(){
+    if(hasNext()){
+      current++;
+      return current-1;
+    }
+    else{
+      throw new NoSuchElementException("No such element exists");
+    }
+  }
+
+}

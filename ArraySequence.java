f8294033511c574ec2e311f43526559f65e961c8
ryan-li20 <rli20@stuy.edu>
2020-12-07 15:47:01 -0500

    working

--- ArraySequence.java
+++ ArraySequence.java
@@ -3,12 +3,28 @@ public class ArraySequence implements IntegerSequence{
   private int currentIndex;
   private int[] data;
 
+  public int[] getData(){
+    return data;
+  }
   /*Construct the sequence by copying values from the other array into the data array*/
   public ArraySequence(int [] other){
     data = new int[other.length];
     for(int i = 0; i < other.length; i++){
       data[i] = other[i];
     }
+    currentIndex = 0;
+  }
+
+  public ArraySequence(IntegerSequence otherseq){
+    data = new int[otherseq.length()];
+    otherseq.reset();
+    currentIndex = 0;
+    while(otherseq.hasNext()){
+      data[currentIndex] = otherseq.next();
+      currentIndex++;
+    }
+    otherseq.reset();
+    currentIndex = 0;
   }
 
   public void reset(){

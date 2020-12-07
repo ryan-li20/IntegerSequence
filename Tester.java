6fec95ae6b5c21b73729f5a60bae4f6406bf288f
ryan-li20 <rli20@stuy.edu>
2020-12-07 13:55:56 -0500

    first constr

--- Tester.java
+++ Tester.java
@@ -1,11 +1,19 @@
 public class Tester{
   public static void main(String[] args){
-    IntegerSequence r = new Range(10,15);
-    while(r.hasNext()){
-      System.out.print(r.next());
-      if( r.hasNext() ){
-         System.out.print( ", " );
-      }
+    // IntegerSequence r = new Range(10,15);
+    // while(r.hasNext()){
+    //   System.out.print(r.next());
+    //   if( r.hasNext() ){
+    //      System.out.print( ", " );
+    //   }
+    // }
+    // System.out.println();
+    int[]nums = {1,3,5,0,-1,3,9};
+    IntegerSequence as = new ArraySequence(nums);
+
+    System.out.println("ArraySequence(array):");
+    while(as.hasNext()){
+      System.out.print(as.next()+", ");
     }
     System.out.println();
   }
